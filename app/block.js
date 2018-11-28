d49bd7e30699b04cdbc700b0e59201a92280b0fd
rbbri <robin.s.johnson@gmail.com>
2018-11-28 22:41:54 +0000

    Add index, nonce, previousHash, and hash
    * calcHash uses SHA256 to calculate the hash

--- app/block.js
+++ app/block.js
@@ -1,8 +1,25 @@
+var sha256 = require('js-sha256')
+
 class Block {
+
   constructor(timestamp, data) {
     this.timestamp = timestamp;
     this.data = data;
+    this.index = 0;
+    this.previousHash = '0'
+    this.nonce = 0;
+    this.hash = this.calcHash()
   }
+
+  calcHash() {
+    return sha256(
+      this.timestamp +
+      this.data +
+      this.index +
+      this.previousHash +
+      this.nonce).toString()
+  }
+
 
 }
 
--- test/blockTest.js
+++ test/blockTest.js
@@ -1,15 +1,16 @@
 let chai = require('chai');
-  path = require('path');
+ path = require('path');
 
   chai.should();
 
 let Block = require(path.join(__dirname, '../app/', 'block'));
 
 describe('Block', () => {
+
   beforeEach(() => {
     date = new Date
     time = date.getTime()
-    block = new Block(time, 'data')
+    block = new Block(time, 'data', this.hash='0')
   });
 
   it('should have a timestamp', () => {
@@ -17,5 +18,20 @@ describe('Block', () => {
   })
   it('should contain data', () => {
     block.data.should.equal('data')
+      })
+  it('should have an index 0', () => {
+    block.index.should.equal(0)
+  })
+  it('should have previous hash 0', () => {
+    block.previousHash.should.equal('0')
+  })
+  it('should have nonce 0', () => {
+    block.nonce.should.equal(0)
+  })
+
+  describe('calcHash', () => {
+    it('should calculate the block hash', () => {
+      block.hash.should.equal(block.calcHash())
+    })
   })
 })
